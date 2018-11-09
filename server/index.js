ba9ee6bfa9ec6692fb9cecea75a8abe8e10dea1e
javaxx <136222866@qq.com>
2018-11-09 17:15:14 +0800

    feat: 增加模版引擎中间件

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const Koa = require('koa')
 const app = new Koa();
+// 引入模版
+const views  = require('koa-views')
+const {resolve} = require('path')
+app.use(views(resolve(__dirname,'./views'),{
+    extension:'pug'
+}))
+
 app.use(async (ctx , next)=>{
-        ctx.body = '首页'    
+    await ctx.render('index',{
+        you:'LUke',
+        me:'numbersi'
+    }) 
 })
-app.listen(4444, () => {
+app.listen(4443, () => {
     console.log('App listening on port 4444!');
 });
